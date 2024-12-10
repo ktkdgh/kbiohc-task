237f750af72d99624bdbec44fba72a0362c8fdf5
ktkdgh <ktkdgh821@gmail.com>
2024-12-10 21:48:02 +0900

    fix: prettier, eslint tapwidth 수정

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     // cors 설정
-    app.enableCors({
-        origin: 'http://localhost:3001',
-    });
+    app.enableCors({ origin: 'http://localhost:3001' });
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
